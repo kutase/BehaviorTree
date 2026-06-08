09c57ccdab9fe586c4c45814d3fbd4fc1b180d9b
kutase <kutase123@gmail.com>
2026-06-08 15:11:32 +0500

    Tree Reset method fixed

--- Runtime/BehaviorTreeRunner.cs
+++ Runtime/BehaviorTreeRunner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Plugins.BehaviorTree.Runtime.Nodes;
 using UnityEngine;
 
@@ -12,37 +13,59 @@ namespace Plugins.BehaviorTree.Runtime
 
         public NodeState TreeState => Tree?.LastState ?? NodeState.NotActive;
 
+        private HashSet<BehaviorTree> ResettingTrees { get; } = new();
+
         public void ReplaceTree(BehaviorTree tree)
         {
+            ResetTreesInternal();
             Tree = tree;
         }
 
         public void StartTree(BehaviorTree tree)
         {
+            ResetTreesInternal();
             Tree = tree;
             Tree.Start();
         }
 
         public void StopTree()
         {
-            if (Tree != null)
+            if (Tree == null)
             {
-                Tree.Reset();
+                return;
             }
 
+            ResettingTrees.Add(Tree);
             Tree = null;
         }
 
         private void Update()
         {
             if (!_runTreeInUpdate)
+            {
                 return;
+            }
 
             if (Tree == null)
+            {
                 return;
+            }
 
             if (TreeState == NodeState.Running)
+            {
                 Tree.Tick();
+            }
+
+            ResetTreesInternal();
+        }
+
+        private void ResetTreesInternal()
+        {
+            foreach (var tree in ResettingTrees)
+            {
+                tree.Reset();
+            }
+            ResettingTrees.Clear();
         }
     }
 }
